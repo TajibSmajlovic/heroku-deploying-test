85b746d247202d2f8df175f4c6252a26245b9036
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-18 11:08:07 +0200

    last commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp').then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Server is up!')
 }, (err) => {
     console.log('Unable to connect', err)
