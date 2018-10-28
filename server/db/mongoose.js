9cfb9cf48573c6277eb57c4c570da05eda2bfca9
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-28 13:11:03 +0100

    node-todo-app commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true }).then(() => {
     console.log('Server is up!')
 }, (err) => {
     console.log('Unable to connect', err)
--- server/server.js
+++ server/server.js
@@ -6,17 +6,19 @@ const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
 
 let {mongoose} = require('./db/mongoose')
-var {Todo} = require('./models/todo')
-var {User} = require('./models/user')
+let {Todo} = require('./models/todo')
+let {User} = require('./models/user')
+let {authenticate} = require('./middleware/authenticate')
 
 let app = express()
 const port = process.env.PORT
 
 app.use(bodyParser.json())
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     let todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     })
 
     todo.save().then((doc) => {
@@ -26,8 +28,10 @@ app.post('/todos', (req, res) => {
     })
 })
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({
+        _creator: req.user._id
+    }).then((todos) => {
         res.send({todos})
     }, (e) => {
         res.status(400).send(e)
@@ -35,14 +39,17 @@ app.get('/todos', (req, res) => {
 })
 
 // GET /todos/12345
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
     let id = req.params.id
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send('ID not valid.')
     }
 
-    Todo.findById(id).then((todo) => {
+    Todo.findOne({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if (!todo) {
             return res.status(404).send('ID not found')
         }
@@ -51,19 +58,22 @@ app.get('/todos/:id', (req, res) => {
     }).catch((err) => res.status(400).send())
 })
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
     let id = req.params.id
 
     if (!ObjectID.isValid(id)) res.status(404).send('ID not valid.')
 
-    Todo.findByIdAndRemove(id).then((todo) => {
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
 
         todo ? res.status(200).send({todo}) : res.status(404).send()
 
     }).catch((err) => res.status(400).send())
 })
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     let id = req.params.id
     let body = _.pick(req.body, ['text', 'completed'])
 
@@ -76,13 +86,48 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send
         }
 
         res.send({todo})
     }).catch((e) => res.status(400).send())
+})
+
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password'])
+    let user = new User(body)
+
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth', token).send(user)
+    }).catch((e) => res.status(400).send(e))
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
+})
+
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password'])
+
+    User.findByCredential(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user)
+        })
+    }).catch((e) => {
+        res.status(400).send()
+    })
+})
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send()
+    }, () => {
+        res.status(400).send()
+    })
 })
 
 app.listen(port, () => {
