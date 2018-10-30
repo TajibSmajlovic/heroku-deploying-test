4e509e6fcd24796907dac664dbb688569b09cf48
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-30 13:25:36 +0100

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,17 +1,32 @@
-const express = require('express')
-
 const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 let app = express()
+let server = http.createServer(app)
+let io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-/*app.get('/', (req,res) => {
+io.on('connection', (socket) => {
+    console.log('New User connected.')
 
-})*/
+    socket.on('createMessage', (message) => {
+        console.log(message)
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+    })
+    socket.on('disconnect', () => {
+        console.log('User was disconnected.')
+    })
+})
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
