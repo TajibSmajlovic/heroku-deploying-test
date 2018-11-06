869074ded65443685f133c202fbe7d9ff67671f2
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-11-06 02:24:02 +0100

    heroku commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+const {generateMessage, generateLocationMessage} = require('./utils/message')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 let app = express()
@@ -14,14 +15,20 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New User connected.')
 
-    socket.on('createMessage', (message) => {
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'))
+
+    socket.on('createMessage', (message, callback) => {
         console.log(message)
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        io.emit('newMessage', generateMessage(message.from, message.text))
+        callback()
     })
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+    })
+
     socket.on('disconnect', () => {
         console.log('User was disconnected.')
     })
